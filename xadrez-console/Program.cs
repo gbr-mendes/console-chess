14034d42a3483971166da1d7cbb06350533a9416
Gabriel Mendes <gabrielx165@gmail.com>
2023-04-30 18:45:47 -0300

    cria classe posição

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Board;
+using System;
+
+
+namespace xadrez_console
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Position P;
+            P = new Position(3, 4);
+            Console.WriteLine($"Posição: {P}");
+        }
+    }
+}
